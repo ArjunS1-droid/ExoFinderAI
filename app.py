fb544c9e27921e484307dc7f315a718c7a3d40bf
ArjunS1-droid <shitolearjun51@gmail.com>
2026-06-25 20:04:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import fits
+st.set_page_config(page_title="ExoFinder AI", layout="wide")
 
 st.markdown("""
 <style>
